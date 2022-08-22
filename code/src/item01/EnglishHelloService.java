11d9d75283907adfb88c2f4256a558f5cd35e072
Ted <dlwnsgus777@naver.com>
2022-08-22 15:50:00 +0900

    feat: update
    feat: update

---
+++ code/src/item01/EnglishHelloService.java
@@ -0,0 +1,8 @@
+package item01;
+
+public class EnglishHelloService implements HelloService {
+   @Override
+   public String hello() {
+     return "Hello";
+   }
+}
---
+++ code/src/item01/Settings.java
@@ -0,0 +1,15 @@
+package item01;
+
+public class Settings {
+
+   private boolean useAutoSteering;
+   private boolean userABS;
+
+   private Settings() {}
+
+   private static final Settings SETTINGS = new Settings();
+
+   public static Settings getInstance() {
+      return SETTINGS;
+   }
+}
