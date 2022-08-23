3494798fe1ffd283dab3e35971aa1d9984371fb9
Ted <dlwnsgus777@naver.com>
2022-08-23 09:34:23 +0900

    feat: 완벽 공략 enum 작성 중
    feat: 완벽 공략 enum 작성 중

---
+++ code/src/item01/Item01Main.java
@@ -0,0 +1,7 @@
+package item01;
+
+public class Item01Main {
+   public static void main(String[] args) {
+
+   }
+}
--- code/src/item01/Order.java
+++ code/src/item01/Order.java
@@ -1,11 +1,16 @@
 package item01;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Order {
 
    private boolean prime;
    private boolean urgent;
+
+   private OrderStatus status;
 
    public static Order primeOrder() {
       Order order = new Order();
@@ -18,5 +23,13 @@ public class Order {
       Order order = new Order();
       order.urgent = true;
       return order;
+   }
+
+   public static void main(String[] args) {
+      Order order = new Order();
+
+      if (order.status.equals()) {
+
+      }
    }
 }
