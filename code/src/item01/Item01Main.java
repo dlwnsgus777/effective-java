39ff58b5220e8c440d299b8ff5e887a89844895d
Ted <dlwnsgus777@naver.com>
2022-08-24 15:53:44 +0900

    feat: 강의 내용 정리 중
    feat: 강의 내용 정리 중

--- code/src/item01/Item01Main.java
+++ code/src/item01/Item01Main.java
@@ -1,7 +1,13 @@
 package item01;
 
+import java.util.EnumMap;
+import java.util.EnumSet;
+import java.util.Map;
+
 public class Item01Main {
    public static void main(String[] args) {
+      Map<OrderStatus, String> enumMap = new EnumMap<>(OrderStatus.class);
 
+      EnumSet<OrderStatus> enumSet = EnumSet.allOf(OrderStatus.class);
    }
 }
