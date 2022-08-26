032edbbf3e8ba67469c43205ccc8a19eedb9b90c
Ted <dlwnsgus777@naver.com>
2022-08-26 14:08:35 +0900

    feat: 리플렉션 정리
    feat: 리플렉션 정리

--- code/src/main/java/effective/code/item01/HelloWorld.java
+++ code/src/main/java/effective/code/item01/HelloWorld.java
@@ -3,10 +3,22 @@ package effective.code.item01;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class HelloWorld {
-   public static void main(String[] args) {
-      ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-      HelloService helloService = applicationContext.getBean(HelloService.class);
-      System.out.println(helloService);
+   public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+//      ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+//      HelloService helloService = applicationContext.getBean(HelloService.class);
+//      System.out.println(helloService);
+
+      Class<?> aClass = Class.forName("effective.code.item01.KoreanHelloService");
+      Constructor<?> constructor = aClass.getConstructor();
+      HelloService helloService = (HelloService) constructor.newInstance();
+      System.out.println(helloService.hello());
+
+      Method[] methods = aClass.getDeclaredMethods();
+
    }
 }
