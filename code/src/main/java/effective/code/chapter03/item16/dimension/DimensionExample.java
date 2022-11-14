a2c1d053e025304cbab4c128b4ca5398e2339655
Ted <dlwnsgus777@naver.com>
2022-11-14 09:40:40 +0900

    feat: 아이템16 완벽 공략 정리 완료
    feat: 아이템16 완벽 공략 정리 완료

---
+++ code/src/main/java/effective/code/chapter03/item16/dimension/DimensionExample.java
@@ -0,0 +1,17 @@
+package effective.code.chapter03.item16.dimension;
+
+import java.awt.*;
+import java.util.Comparator;
+import java.util.function.BiFunction;
+
+public class DimensionExample {
+
+    public static void main(String[] args) {
+        Button button = new Button("hello button");
+        button.setBounds(0, 0, 20, 10);
+
+        Dimension size = button.getSize();
+        System.out.println(size.height);
+        System.out.println(size.width);
+    }
+}
