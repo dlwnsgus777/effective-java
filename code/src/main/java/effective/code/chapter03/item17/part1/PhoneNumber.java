0d6ae17068da3c420a81c4b87b076aac20250b0e
Ted <dlwnsgus777@naver.com>
2022-11-17 16:13:21 +0900

    feat: 아이템 17 핵심정리 정리 중
    feat: 아이템 17 핵심정리 정리 중

---
+++ code/src/main/java/effective/code/chapter03/item17/part1/PhoneNumber.java
@@ -0,0 +1,24 @@
+package effective.code.chapter03.item17.part1;
+
+public final class PhoneNumber {
+
+    private short areaCode, prefix, lineNum;
+
+    public PhoneNumber(short areaCode, short prefix, short lineNum) {
+        this.areaCode = areaCode;
+        this.prefix = prefix;
+        this.lineNum = lineNum;
+    }
+
+    public short getAreaCode() {
+        return areaCode;
+    }
+
+    public short getPrefix() {
+        return prefix;
+    }
+
+    public short getLineNum() {
+        return lineNum;
+    }
+}
