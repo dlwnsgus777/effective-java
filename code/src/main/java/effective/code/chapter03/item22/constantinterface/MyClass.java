74b5c34791c02830c2c0b8e86d9ba32708244ca2
Ted <dlwnsgus777@naver.com>
2022-11-30 08:42:59 +0900

    feat: 아이템 22 핵심 정리 정리 완료
    feat: 아이템 22 핵심 정리 정리 완료

---
+++ code/src/main/java/effective/code/chapter03/item22/constantinterface/MyClass.java
@@ -0,0 +1,9 @@
+package effective.code.chapter03.item22.constantinterface;
+
+public class MyClass implements PhysicalConstants {
+
+    public static void main(String[] args) {
+        System.out.println(BOLTZMANN_CONSTANT);
+    }
+
+}
