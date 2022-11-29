4f7036314f1c5c010ad6b85f21a601d799404ed2
Ted <dlwnsgus777@naver.com>
2022-11-29 09:11:32 +0900

    feat: 아이템20 완벽 공략 정리 완료
    feat: 아이템20 완벽 공략 정리 완료

--- code/src/main/java/effective/code/chapter03/item20/templatemethod/Client.java
+++ code/src/main/java/effective/code/chapter03/item20/templatemethod/Client.java
@@ -5,5 +5,8 @@ public class Client {
     public static void main(String[] args) {
         FileProcessor fileProcessor = new Plus("number.txt");
         System.out.println(fileProcessor.process());
+
+        FileProcessorCallBack fileProcessorCallBack = new FileProcessorCallBack("number.txt");
+        System.out.println(fileProcessorCallBack.process(Integer::sum));
     }
 }
