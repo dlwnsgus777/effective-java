007e372922e420beeb9989173ca5e7832c7f370d
Ted <dlwnsgus777@naver.com>
2022-12-02 13:44:55 +0900

    feat: 아이템 25 핵심 정리 정리 완료
    feat: 아이템 25 핵심 정리 정리 완료

---
+++ code/src/main/java/effective/code/chapter03/item25/Main.java
@@ -0,0 +1,8 @@
+package effective.code.chapter03.item25;
+
+// (150쪽)
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Utensil.NAME + Dessert.NAME);
+    }
+}
